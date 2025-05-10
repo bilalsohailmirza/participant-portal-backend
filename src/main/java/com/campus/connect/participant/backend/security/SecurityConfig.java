b60827b690520ed6f04f55ec3bfbfe382edd3a98
Hamza Iqbal <chander.kumar@techlogix.com>
2025-05-11 02:00:24 +0500

    cors allowed

--- src/main/java/com/campus/connect/participant/backend/security/SecurityConfig.java
+++ src/main/java/com/campus/connect/participant/backend/security/SecurityConfig.java
@@ -5,12 +5,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import com.campus.connect.participant.backend.security.jwt.AuthTokenFilter;
 import com.campus.connect.participant.backend.security.services.CustomUserDetailsService;
 import org.springframework.security.authentication.AuthenticationManager;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -51,6 +57,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+            .cors(Customizer.withDefaults())
             .authorizeHttpRequests( auth -> 
             auth.requestMatchers("api/users/signup").permitAll()
             .requestMatchers("api/users/login").permitAll()
@@ -71,6 +78,17 @@ public class SecurityConfig {
         return http.build();
     }
 
+        @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*"); // Allow all origins
+        configuration.addAllowedMethod("*");        // Allow all HTTP methods
+        configuration.addAllowedHeader("*");        // Allow all headers
+        configuration.setAllowCredentials(true);    // Allow credentials (optional)
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     
 }
 
